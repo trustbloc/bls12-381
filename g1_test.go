ea725ae421b82068c77deda05e0867f9a543b456
kilic <kiliconu@itu.edu.tr>
2020-10-15 14:36:24 +0300

    add math comments
    use size constants
    
    fix method and test comments

--- g1_test.go
+++ g1_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func (g *G1) one() *PointG1 {
-	one, _ := g.fromBytesUnchecked(fromHex(48,
+	one, _ := g.fromBytesUnchecked(fromHex(fpByteSize,
 		"0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb",
 		"0x08b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1",
 	))
@@ -36,7 +36,7 @@ func TestG1Serialization(t *testing.T) {
 		t.Fatal(err)
 	}
 	if !g1.IsZero(p0) {
-		t.Fatal("bad infinity serialization 1")
+		t.Fatal("infinity serialization failed")
 	}
 	b0 = g1.ToCompressed(zero)
 	p0, err = g1.FromCompressed(b0)
@@ -44,7 +44,7 @@ func TestG1Serialization(t *testing.T) {
 		t.Fatal(err)
 	}
 	if !g1.IsZero(p0) {
-		t.Fatal("bad infinity serialization 2")
+		t.Fatal("infinity serialization failed")
 	}
 	b0 = g1.ToBytes(zero)
 	p0, err = g1.FromBytes(b0)
@@ -52,7 +52,7 @@ func TestG1Serialization(t *testing.T) {
 		t.Fatal(err)
 	}
 	if !g1.IsZero(p0) {
-		t.Fatal("bad infinity serialization 3")
+		t.Fatal("infinity serialization failed")
 	}
 	for i := 0; i < fuz; i++ {
 		a := g1.rand()
@@ -62,7 +62,7 @@ func TestG1Serialization(t *testing.T) {
 			t.Fatal(err)
 		}
 		if !g1.Equal(a, b) {
-			t.Fatal("bad serialization 1")
+			t.Fatal("serialization failed")
 		}
 		compressed := g1.ToCompressed(b)
 		a, err = g1.FromCompressed(compressed)
@@ -70,7 +70,7 @@ func TestG1Serialization(t *testing.T) {
 			t.Fatal(err)
 		}
 		if !g1.Equal(a, b) {
-			t.Fatal("bad serialization 2")
+			t.Fatal("serialization failed")
 		}
 	}
 	for i := 0; i < fuz; i++ {
@@ -81,7 +81,7 @@ func TestG1Serialization(t *testing.T) {
 			t.Fatal(err)
 		}
 		if !g1.Equal(a, b) {
-			t.Fatal("bad serialization 3")
+			t.Fatal("serialization failed")
 		}
 	}
 }
@@ -299,14 +299,14 @@ func TestZKCryptoVectorsG1UncompressedValid(t *testing.T) {
 	g := NewG1()
 	p1 := g.Zero()
 	for i := 0; i < 1000; i++ {
-		vector := data[i*96 : (i+1)*96]
+		vector := data[i*2*fpByteSize : (i+1)*2*fpByteSize]
 		p2, err := g.FromUncompressed(vector)
 		if err != nil {
 			t.Fatal("decoing fails", err, i)
 		}
 		uncompressed := g.ToUncompressed(p2)
 		if !bytes.Equal(vector, uncompressed) || !g.Equal(p1, p2) {
-			t.Fatal("bad serialization")
+			t.Fatal("serialization failed")
 		}
 
 		g.Add(p1, p1, &g1One)
@@ -321,14 +321,14 @@ func TestZKCryptoVectorsG1CompressedValid(t *testing.T) {
 	g := NewG1()
 	p1 := g.Zero()
 	for i := 0; i < 1000; i++ {
-		vector := data[i*48 : (i+1)*48]
+		vector := data[i*fpByteSize : (i+1)*fpByteSize]
 		p2, err := g.FromCompressed(vector)
 		if err != nil {
 			t.Fatal("decoing fails", err, i)
 		}
 		compressed := g.ToCompressed(p2)
 		if !bytes.Equal(vector, compressed) || !g.Equal(p1, p2) {
-			t.Fatal("bad serialization")
+			t.Fatal("serialization failed")
 		}
 		g.Add(p1, p1, &g1One)
 	}
@@ -346,7 +346,7 @@ func TestG1MultiExpBigExpected(t *testing.T) {
 	g.MulScalarBig(expected, one, big.NewInt(5))
 	_, _ = g.MultiExpBig(result, bases[:], scalars[:])
 	if !g.Equal(expected, result) {
-		t.Fatal("bad multi-exponentiation")
+		t.Fatal("multi-exponentiation failed")
 	}
 }
 
@@ -362,7 +362,7 @@ func TestG1MultiExpExpected(t *testing.T) {
 	g.MulScalar(expected, one, &Fr{5})
 	_, _ = g.MultiExp(result, bases[:], scalars[:])
 	if !g.Equal(expected, result) {
-		t.Fatal("bad multi-exponentiation")
+		t.Fatal("multi-exponentiation failed")
 	}
 }
 
@@ -387,7 +387,7 @@ func TestG1MultiExpBig(t *testing.T) {
 		result := g.New()
 		_, _ = g.MultiExpBig(result, bases, scalars)
 		if !g.Equal(expected, result) {
-			t.Fatal("bad multi-exponentiation")
+			t.Fatal("multi-exponentiation failed")
 		}
 	}
 }
@@ -413,7 +413,7 @@ func TestG1MultiExp(t *testing.T) {
 		result := g.New()
 		_, _ = g.MultiExp(result, bases, scalars)
 		if !g.Equal(expected, result) {
-			t.Fatal("bad multi-exponentiation")
+			t.Fatal("multi-exponentiation failed")
 		}
 	}
 }
@@ -424,7 +424,7 @@ func TestG1MapToCurve(t *testing.T) {
 		expected []byte
 	}{
 		{
-			u: make([]byte, 48),
+			u: make([]byte, fpByteSize),
 			expected: fromHex(-1,
 				"11a9a0372b8f332d5c30de9ad14e50372a73fa4c45d5f2fa5097f2d6fb93bcac592f2e1711ac43db0519870c7d0ea415",
 				"092c0f994164a0719f51c24ba3788de240ff926b55f58c445116e8bc6a47cd63392fd4e8e22bdf9feaa96ee773222133",
@@ -675,7 +675,7 @@ func BenchmarkG1MultiExpAffine(t *testing.B) {
 }
 
 func BenchmarkG1MapToCurve(t *testing.B) {
-	a := fromHex(48, "0x1234")
+	a := fromHex(fpByteSize, "0x1234")
 	g1 := NewG1()
 	t.ResetTimer()
 	for i := 0; i < t.N; i++ {
